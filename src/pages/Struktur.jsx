033e6095c2001ca2d81c0ae7a66ee1cb177a5d70
Jiwa2 <mjiwaislamutidar@gmail.com>
2026-04-14 08:29:12 +0700

    update baru

--- src/pages/Struktur.jsx
+++ src/pages/Struktur.jsx
@@ -47,15 +47,15 @@ export default function Struktur() {
         <div className="bph-wrapper">
 
           <div className="bph-top">
-            <Card nama="Nama" jabatan="Ketua Umum" foto="/images/user.png" />
-            <Card nama="Nama" jabatan="Wakil Ketua Umum" foto="/images/user.png" />
+            <Card nama="Saldy Saputra" jabatan="Ketua Umum" foto="/images/Saldy Saputra .png" />
+            <Card nama="Fat Khudin" jabatan="Wakil Ketua Umum" foto="/images/himtif/Fat khudin.png" />
           </div>
 
           <div className="bph-bottom">
-            <Card nama="Nama" jabatan="Sekretaris Umum 1" foto="/images/ketua.jpg" />
-            <Card nama="Nama" jabatan="Sekretaris Umum 2" foto="/images/wakil.jpg" />
-            <Card nama="Nama" jabatan="Bendahara umum 1" foto="/images/user.png" />
-            <Card nama="Nama" jabatan="Bendahara umum 2" foto="/images/user.png" />
+            <Card nama="Nurlayla Fitri" jabatan="Sekretaris Umum 1" foto="/images/himtif/Nurlayla fitri.png" />
+            <Card nama="Aulia Putri" jabatan="Sekretaris Umum 2" foto="/images/himtif/Aulia Putri .png" />
+            <Card nama="Medina Fikanti" jabatan="Bendahara umum 1" foto="/images/himtif/Medina Fikanti.png" />
+            <Card nama="Amelia Zahwa H.S" jabatan="Bendahara umum 2" foto="/images/himtif/Amelia Zahwa Habilla Syifa.png" />
           </div>
 
         </div>
@@ -64,24 +64,47 @@ export default function Struktur() {
 
         <div className="grid">
 
-          <Card nama="Nama" jabatan="Kepala Department Ristek" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Kepala Department Humas" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Kepala Department Minfo" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Kepala Department Sosbud" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Kepala Department PSDM" foto="/images/user.png" />
+          <Card nama="Muhammad Jiwa Islamutidar" jabatan="Kepala Department Ristek" foto="/images/ristek/Muhammad Jiwa Islamutidar.png" />
+          <Card nama="Arkananta Emier Pradipta" jabatan="Kepala Department Humas" foto="/images/humas/ARKANANTA EMIER PRADIPTA.png" />
+          <Card nama="Bagas Anwar Meiqadr" jabatan="Kepala Department Minfo" foto="/images/minfo/BAGAS ANWAR MEIQADR.png" />
+          <Card nama="Farid Mardan Aziz" jabatan="Kepala Department Sosbud" foto="/images/sosbud/Farid Mardan Aziz.png" />
+          <Card nama="Rifky Ramadhan" jabatan="Kepala Department PSDM" foto="/images/PSDM/Rifky Ramadhan Putra Permana.png" />
 
-          <Card nama="Nama" jabatan="Sekretaris Department" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Sekretaris Department" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Sekretaris Department" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Sekretaris Department" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Sekretaris Department" foto="/images/user.png" />
+          <Card nama="Ahmad Renaldy" jabatan="Sekretaris Department" foto="/images/ristek/Ahmad Renaldy.png" />
+          <Card nama="Zaydan Azfar" jabatan="Sekretaris Department" foto="/images/humas/ZAYDAN AZFAR.png" />
+          <Card nama="Lidya Septia Nita Darmawati" jabatan="Sekretaris Department" foto="/images/minfo/LIDYA SEPTIA NITA DARMAWATI.png" />
+          <Card nama="Aldi Athalla Putra" jabatan="Sekretaris Department" foto="/images/sosbud/Aldi Athalla Putra.png" />
+          <Card nama="Bilqis Nilam Cahya" jabatan="Sekretaris Department" foto="/images/PSDM/Bilqis Nilam Cahya.png" />
+
+          <Card nama="Mohammad Arief Ardiansyah" jabatan="Staff" foto="/images/ristek/Mohammad Arief Ardiansyah.png" />
+          <Card nama="Raven Firgiasyah" jabatan="Staff" foto="/images/humas/RAVEN FIRGIASYAH.png" />
+          <Card nama="Ika Auliya Rusyadah" jabatan="Staff" foto="/images/minfo/IKA AULIYA RUSYADAH.png" />
+          <Card nama="Deden Jamaludin" jabatan="Staff" foto="/images/sosbud/26.png" />
+          <Card nama="Bilqis Nilam Cahya" jabatan="Staff" foto="/images/PSDM/Bilqis Nilam Cahya.png" />
+
+          <Card nama="Cintya Laura Melagro" jabatan="Staff" foto="/images/ristek/Cintya Laura Melagro.png" />
+          <Card nama="Sihabudin Firmansyah" jabatan="Staff" foto="/images/humas/SIHABUDIN FIRMANSYAH.png" />
+          <Card nama="Dita Adelia Putri" jabatan="Staff" foto="/images/minfo/DITA ADELIA PUTRI.png" />
+          <Card nama="Amirul Mukminin" jabatan="Staff" foto="/images/sosbud/Amirul Mukminin.png" />
+          <Card nama="Niko Putra Yusanto" jabatan="Staff" foto="/images/PSDM/Niko Putra Yusanto.png" />
+
+          <Card nama="Abu Ziyad Al Katani" jabatan="Staff" foto="/images/ristek/Abu Ziyad Al Katani.png" />
+          <Card nama="Muhammad Rafli Firdaus" jabatan="Staff" foto="/images/humas/MUHAMMAD RAFLI FIRDAUS.png" />
+          <Card nama="Olivia Ramadhani" jabatan="Staff" foto="/images/minfo/OLIVIA RAMADHANI.png" />
+          <Card nama="Muhammad Edward" jabatan="Staff" foto="/images/sosbud/Muhamad Edward.png" />
+          <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
+
+          <Card nama="..." jabatan="Staff" foto="/images/.png" />
+          <Card nama="Paris Nurfadhillah" jabatan="Staff" foto="/images/humas/PARIS NURFADHILLAH.png" />
+          <Card nama="Suci Oktaviani" jabatan="Staff" foto="/images/minfo/SUCI OKTAVIANI.png" />
+          <Card nama="Ugem Zilullah" jabatan="Staff" foto="/images/sosbud/Ugem Zilullah.png" />
+          <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
 
           <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
           <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
           <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
+          <Card nama="Arnta Salsabilla" jabatan="Staff" foto="/images/sosbud/Arinta Salsabilla.png" />
           <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
-          <Card nama="Nama" jabatan="Staff" foto="/images/user.png" />
-
         </div>
 
       </div>
